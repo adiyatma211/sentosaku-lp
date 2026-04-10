ef20e0aea4b8018b51c237f2ae0aef55c3fe53bc
adiyatma211 <adiyatmaharis8@gmail.com>
2026-04-10 23:09:36 +0700

    Static Web

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ const nextConfig: NextConfig = {
         pathname: '/projects/**',
       },
       {
-        protocol: 'https',
+        protocol: 'http',
         hostname: 'dashboard.sentosakutech.com',
         port: '',
         pathname: '/**',
